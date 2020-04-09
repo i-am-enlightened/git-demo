613c127395e9964f180109355354d5435e0463d6
i-am-enlightened <31614408+i-am-enlightened@users.noreply.github.com>
2020-04-09 22:48:11 +0530

    created app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+function add (a,b){
+  return a+b
+  }
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+var x = add(12,14)
+console.log(x)
